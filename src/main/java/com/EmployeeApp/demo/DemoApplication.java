f79d6d0df1c5ac18749d5bdeba34e98f7b947efc
tcross-IBM <tcross@ibm.com>
2024-06-13 14:22:37 -0400

    dependency issues fixed

--- src/main/java/com/EmployeeApp/demo/DemoApplication.java
+++ src/main/java/com/EmployeeApp/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.EmployeeApp.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
